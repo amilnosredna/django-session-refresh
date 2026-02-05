6c3e3f04dd855c697bc840c9bc8058ff9d797550
リマ・アンデルソン <152332469+amilnosredna@users.noreply.github.com>
2026-02-05 15:25:13 +0900

    Create pyproject.toml and add GitHub Actions workflow for PyPI publishing
    * Create pyproject.toml for django-session-refresh
    
    * Add GitHub Actions workflow for PyPI publishing
    
    * Add Django requirement to requirements.txt
    
    * Refactor setup.py for direct setup function call

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="django-session-refresh",
-    version="0.0.1",
-    author="Anderson Lima",
-    author_email="amilnosredna@gmail.com",
-    description="A simple Django middleware to refresh user sessions periodically to prevent timeout during active use.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/amilnosredna/django-session-refresh/",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: Apache 2.0",
-        "Operating System :: OS Independent",
-        "Framework :: Django 5+",
-        "Intended Audience :: Developers",
-    ],
-    python_requires='>=3.12',
-)
+if __name__ == "__main__":
+    setup()
